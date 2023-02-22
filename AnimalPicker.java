f3ecc76ca297883b15f7af9c7415680893111bd2
Centeno <ntcenteno@quinnipiac.edu>
2023-02-21 20:31:12 -0500

    Added Invalid Message with a repeater

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class AnimalPicker
 {
     public static void main(String[] args)
     {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which animal would you like to see: a dog or a cat");
+        String animal = scanner.next();
+        while (animal != "dog" || animal != "cat"){
+        System.out.println("Invalid Input");
+        animal = scanner.next();
+       }
+        if(animal == "dog"){
+            System.out.println("DOG ASCII ART HERE");
+        }else if (animal == "cat"){
+            System.out.println("CAT ASCII ART HERE");
+        }
     }
 }
