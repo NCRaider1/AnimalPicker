a307ee6caccc61d3e53d507a5583d4ff63d0b036
Centeno <ntcenteno@quinnipiac.edu>
2023-02-21 19:54:45 -0500

    Can request for cat or dog

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class AnimalPicker
 {
     public static void main(String args)
     {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which animal would you like to see: a dog or a cat");
+        String animal = scanner.next();
+        if(animal == "dog"){
+            System.out.println("DOG ASCII ART HERE");
+        }
+        else if (animal == "cat"){
+            System.out.println("CAT ASCII ART HERE");
+        }
     }
 }
