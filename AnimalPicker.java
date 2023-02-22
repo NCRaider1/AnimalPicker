0a50f30cb6cc28c47d96e41b84755d95f546977e
Centeno <ntcenteno@quinnipiac.edu>
2023-02-21 21:47:41 -0500

    Added fish

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 public class AnimalPicker
 {
     public static void main(String[] args)
     {
-
+        Scanner scanner = new Scanner(System.in);
+        boolean statement = true;
+        while(statement)
+        {                                                    
+        System.out.println("Which animal would you like to see: a dog, a cat, or a fish");
+        String animal = scanner.next();                                                                  
+        if (animal.equalsIgnoreCase("dog"))
+        {
+            System.out.println("  __      _\no\'\')}____//\n `_/      )\n (_(_/-(_/");
+            statement = false;
+        }
+        else if (animal.equalsIgnoreCase("cat"))
+        {
+            System.out.println(" _._     _,-\'\"\"`-._\n(,-.`._,\'(       |\\`-/|\n    `-.-\' \\ )-`( , o o)\n ");
+            statement = false;
+        }
+        else if (animal.equalsIgnoreCase("fish"))
+        {
+            System.out.println("          /\"*._         _\n      .-*\'`    `*-.._.-\'/\n    < * ))     ,       ( \n      `*-._`._(__.--*\"`.\\");
+            statement = false;                                              
+        }
+        else
+        {
+            System.out.println("Invalid Input");
+        }
+        }
     }
 }
